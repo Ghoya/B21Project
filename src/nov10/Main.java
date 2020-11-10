4d39742ec39d5e77f032010f78a691880a7289bc
Ghoya <58498238+Ghoya@users.noreply.github.com>
2020-11-10 09:19:34 -0800

    initial commit

--- src/nov10/Main.java
+++ src/nov10/Main.java
@@ -5,7 +5,8 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello ");
-
+        System.out.println("\"Hello Batch 21\" = " + "Hello Batch 21");
+//I am doing good
 
     }
 }
